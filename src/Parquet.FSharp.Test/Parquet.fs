5fde94bda039d1177451703676749e1639ee0303
Chris Bremer <cbremer4@gmail.com>
2024-06-29 10:59:42 -0500

    Updating language

--- src/Parquet.FSharp.Test/Parquet.fs
+++ src/Parquet.FSharp.Test/Parquet.fs
@@ -21,9 +21,8 @@ type CompactType =
 
 type ThriftState = {
     stream: Stream
-    lastField: int16 list
-    lastFieldId: int16
-}
+    stack: int16 list
+    }
 module Stream =
     let readInt32Async(stream: Stream) =
         async {
@@ -49,79 +48,110 @@ module Stream =
             return tmp
         }
 module ThriftCompact =
-    let beginStruct (state: ThriftState) =
+    let private beginStruct (state: ThriftState) =
         { state with
-            lastField = state.lastFieldId :: state.lastField
-            lastFieldId = 0s
+            stack = 0s :: state.stack
         }
-    let endStruct (state: ThriftState) =
-        match state.lastField with
-        | lastFieldId :: rest -> { state with lastField = rest; lastFieldId = lastFieldId }
+    let private endStruct (state: ThriftState) =
+        match state.stack with
+        | _ :: rest -> { state with stack = rest }
         | _ -> InvalidOperationException("Malformed Thrift stream") |> raise
     
     let private zigZagToInt32 (n: uint) =
         int (n >>> 1) ^^^ (-(int n &&& 1))
     let private zigZagToInt64 (n: uint64) =
         int64 (n >>> 1) ^^^ (-(int64 n &&& 1))
-    let private readVarInt32 (stream: Stream ) =
+    let private readVarInt32 (state: ThriftState) =
         let rec loop acc shift =            
-            let b = stream.ReadByte() |> byte
+            let b = state.stream.ReadByte() |> byte
             let acc = acc ||| (uint (b &&& 0x7fuy) <<< shift) 
             if b &&& 0x80uy <> 0x80uy then
                 acc
             else
                 loop acc (shift + 7)
-        loop 0u 0, stream
-    let private readVarInt64 (stream: Stream) =
+        loop 0u 0, state
+    let private readVarInt64 (state: ThriftState) =
         let rec loop acc shift =            
-            let b = stream.ReadByte() |> byte
+            let b = state.stream.ReadByte() |> byte
             let acc = acc ||| (uint64 (b &&& 0x7fuy) <<< shift) 
             if b &&& 0x80uy <> 0x80uy then
                 acc
             else
                 loop acc (shift + 7)
-        loop 0UL 0, stream
-    let private readByte (stream: Stream) =
-        stream.ReadByte() |> sbyte, stream
+        loop 0UL 0, state
+    let private readByte (state: ThriftState) =
+        state.stream.ReadByte() |> sbyte, state
     
-    let private readI16 (stream: Stream) =
-        let i, s = readVarInt32 stream
-        zigZagToInt32 i |> int16, stream
-    
-    let private readI32 (stream: Stream) =
-        let i, s = (readVarInt32 stream)
+    let private readI16 (state: ThriftState) =
+        let i, s = readVarInt32 state
+        zigZagToInt32 i |> int16, s
+    let private swapField state =
+        let a,newState = readI16 state
+        match newState.stack with
+        | _ :: rest -> {newState  with stack = a :: rest}
+        | _ -> {state with stack = [a]}
+    let private fieldDelta b state =
+        match state.stack with
+        | a :: rest -> {state with stack = (a+b) :: rest}
+        | _ -> {state with stack = [b]}        
+    let private readI32 (state: ThriftState) =
+        let i, s = (readVarInt32 state)
         zigZagToInt32 i, s
     
-    let private readI64 (stream: Stream) =
-        let i, s = (readVarInt64 stream)
+    let private readI64 (state: ThriftState) =
+        let i, s = (readVarInt64 state)
         zigZagToInt64 i, s
     
-    let private readBinary (stream: Stream) =
-        let uLen, s = readVarInt32 stream
+    let private readBinary (state: ThriftState) =
+        let uLen, s = readVarInt32 state
         let len = int uLen
         if (len = 0 ) then
             [||], s
         else
-            Stream.readBytes len s, s
+            Stream.readBytes len s.stream, s
    
-    let private readString (stream: Stream) =
-        let uLen, s = readVarInt32 stream
+    let private readString (state: ThriftState) =
+        let uLen, s = readVarInt32 state
         let len = int uLen
         if (len = 0 ) then
             String.Empty, s
         else
-            System.Text.Encoding.UTF8.GetString(Stream.readBytes len s, 0, len)
+            System.Text.Encoding.UTF8.GetString(Stream.readBytes len s.stream, 0, len)
             , s
     
-    let private readListHeader (stream: Stream) =
-        let sizeAndType = stream.ReadByte()
+    let private readListHeader (state: ThriftState) =
+        let sizeAndType = state.stream.ReadByte()
         let size = sizeAndType >>> 4 &&& 0x0
         if size = 15 then
-            let size, s = readVarInt32 stream
+            let size, s = readVarInt32 state
             int size, s
         else
-            int size, stream
-    
+            int size, state
+    let (|FieldId|) state =
+        match state.stack with
+        | a :: rest -> a, state
+        | _ -> 0s, state
+    let (|StructField|ExitStruct|) state =
+        let header = state.stream.ReadByte()
+        if enum header = CompactType.Stop then
+            ExitStruct
+        else
+            let modifier = header &&& 0xf0 >>> 4 |> int16
+            let compactType = header &&& 0x0f
+            if modifier = 0s then
+                StructField (compactType, swapField state)
+            else
+                StructField (compactType, fieldDelta modifier state)
+                
+            
+                    
+                    
+            
+                
+            
+                
+        
+        
 
         
           
