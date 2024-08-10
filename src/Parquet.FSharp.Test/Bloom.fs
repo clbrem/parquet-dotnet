d936d52cba6fce626d073f1e2bfdbc8ddf9f6cf5
Chris Bremer <cbremer4@gmail.com>
2024-08-10 00:53:12 -0500

    Added Block operations

--- src/Parquet.FSharp.Test/Bloom.fs
+++ src/Parquet.FSharp.Test/Bloom.fs
@@ -1,10 +1,13 @@
 namespace Parquet
 open System
-open System.IO.Hashing
+
 type Salt = private Salt of uint32 []
 type SaltException() = inherit Exception("Salt can only be built from 8 odd uint32 values.")
-module Salt =
-
+type Block = uint32 []
+module Salt =    
+    let mul (Salt salt) a : Block=
+        [| for s in salt do yield a * s |]
+        
     let rndUint32 (rnd: Random) =
         let i = rnd.Next( 1 <<< 30) |> uint32
         let j = rnd.Next( 1 <<< 1) |> uint32
@@ -19,6 +22,25 @@ module Salt =
         | _ -> raise (SaltException())
     let random (rnd: Random) =
         Array.init 8 (fun _ -> rndUint32 rnd) |> ofArray
-type Block = uint32 []
-    
 
+
+module Block =
+    let empty = Array.zeroCreate 8
+    
+    let mask (salt: Salt) (x: uint32) : Block =
+         Salt.mul salt x
+         |> Array.map2 (
+                 fun y ->
+                    (|||) (1u <<< (int32 (y >>> 27) )) 
+             ) 
+         <| empty
+         
+    let insert (salt: Salt) (block: Block ) x : Block =
+        mask salt x
+        |> Array.map2 (|||) block
+
+    let check (salt: Salt) (block: Block) x =
+        mask salt x
+        |> Array.map2 (&&&) block
+        |> Array.contains 0u
+        |> not
